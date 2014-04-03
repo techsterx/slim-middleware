7836625d0f9dac81821bdeb5e2f6203527388d42
techster <techster@pp-shared-001.aptushosting.net>
2014-04-03 15:12:37 -0500

    Added RequiresHttps

---
+++ RequiresHttps.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TechsterX\Middleware;
+
+class RequiresHttps extends \Slim\Middleware
+{
+	protected $methods;
+
+	public function __constrcut(array $methods = array())
+	{
+		$this->methods = $methods;
+	}
+
+	public function call()
+	{
+		$method = $this->app->request()->getmethod();
+
+		if (count($this->methods) == 0 || in_array($method, $this->methods)) {
+			if ($this->app->environment['slim.url_scheme']  != 'https') {
+				$this->app->response()->setStatus(501);
+				return;
+			}
+		}
+
+		$this->next->call();
+	}
+}

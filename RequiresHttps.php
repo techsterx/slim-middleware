9c306f29edafc57ba5fa521aad0d357fea0c578c
Bill Rex <rexw3035@gmail.com>
2014-04-04 16:56:47 -0500

    Added code for status 426, which slim doesn't currently support

--- RequiresHttps.php
+++ RequiresHttps.php
@@ -17,6 +17,10 @@ class RequiresHttps extends \Slim\Middleware
 
 		if (count($this->methods) == 0 || in_array($method, $this->methods)) {
 			if ($this->app->environment['slim.url_scheme']  != 'https') {
+				// Status 426 isn't currently support by Slim :(
+				//$this->app->response()->setStatus(426);
+				//$this->app->response()->header('Upgrade', 'TLS/1.0, HTTP/1.1');
+				//$this->app->response()->header('Connection', 'Upgrade');
 				$this->app->response()->setStatus(501);
 				return;
 			}
